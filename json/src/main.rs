97b45c613972cedb87316073cb1ba7a9d91fb291
Nihanglama <nihanglama@gmail.com>
2023-10-09 22:58:21 +0545

    json

---
+++ json/src/main.rs
@@ -0,0 +1,43 @@
+use serde::{Deserialize,Serialize};
+
+
+#[derive(Serialize,Deserialize)]
+struct Paragraph{
+    name:String
+}
+
+#[derive(Serialize,Deserialize)]
+struct Article{
+    article:String,
+    author:String,
+    paragraph: Vec<Paragraph>
+
+}
+
+fn main(){
+
+    let json = r#"{
+        "article":"working with json",
+        "author":"Nihang",
+        "paragraph":[
+            {
+                "name":"start"
+
+        },
+        {
+            "name":"end"
+        }
+        ]
+
+
+    }"#;
+
+    let parsed :Article=read_json_typed(json);
+    println!("\n\n The name of the first paragrah is {}",parsed.paragraph[0].name);
+
+}
+
+fn read_json_typed(raw_json:&str)-> Article {
+    let parsed :Article= serde_json::from_str(raw_json).unwrap();
+    return parsed;
+}
